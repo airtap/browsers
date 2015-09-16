19a827a1b2347b936e8c75c5596d287d0cbfef40
vvo <vincent.voyer@algolia.com>
2015-09-17 01:44:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// this is a list of browsers to test against when using https://github.com/defunctzombie/zuul
+// '-2..3.1' means 3.1, 3.0, 2.9
+
+export default {
+  all: [{
+    name: 'chrome',
+    version: '-1..latest',
+    platform: 'Windows 10'
+  }, {
+    name: 'firefox',
+    version: '-1..latest',
+    platform: 'Windows 10'
+  }, {
+    name: 'internet explorer',
+    version: '8..11'
+  }, {
+    name: 'safari',
+    version: '-3..latest'
+  }, {
+    name: 'iphone',
+    version: ['7.1', '-1..8.4', '-1..latest']
+  }, {
+    name: 'android',
+    version: ['-1..4.4', '-1..latest']
+  }, {
+    name: 'ipad',
+    version: ['7.1', '-1..8.4', '-1..latest']
+  }],
+  pullRequest: [{
+    name: 'chrome',
+    version: 'latest',
+    platform: 'Windows 10'
+  }, {
+    name: 'internet explorer',
+    version: '11'
+  }, {
+    name: 'firefox',
+    version: 'latest',
+    platform: 'Windows 10'
+  }, {
+    name: 'iphone',
+    version: 'latest'
+  }, {
+    name: 'android',
+    version: 'latest'
+  }]
+};
